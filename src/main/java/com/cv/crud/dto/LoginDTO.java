c9df4b3efa2122c3d6af3120596711fb6bb4d929
Leonardo Bravo <leonardo.bravo@sociallearning.io>
2023-11-21 03:47:21 -0300

    Added more tests for JWT authentication

--- src/main/java/com/cv/crud/dto/LoginDTO.java
+++ src/main/java/com/cv/crud/dto/LoginDTO.java
@@ -1,10 +1,12 @@
 package com.cv.crud.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class LoginDTO {
 
     private String username;
--- src/test/java/com/cv/crud/controller/BlogControllerTest.java
+++ src/test/java/com/cv/crud/controller/BlogControllerTest.java
@@ -1,23 +1,33 @@
 package com.cv.crud.controller;
 
-import com.cv.crud.configuration.SecurityConfig;
+import com.cv.crud.dto.LoginDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@EnableWebMvc
-@Import({SecurityConfig.class})
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 @AutoConfigureMockMvc
 class BlogControllerTest {
 
     @Autowired
     MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     void whenUnauthenticatedThen401() throws Exception {
@@ -26,6 +36,26 @@ class BlogControllerTest {
 
     @Test
     void whenAuthorizedThen200() throws Exception {
+
+        LoginDTO loginDTO = new LoginDTO("leo", "casona");
+        String body = objectMapper.writeValueAsString(loginDTO);
+
+        MvcResult result = mvc.perform(post("/token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String token = result.getResponse().getContentAsString();
+
+        mvc.perform(get("/").header("Authorization", "Bearer " + token))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello!"));
+    }
+
+    @Test
+    @WithMockUser
+    void withAuthenticatedUserThen200() throws Exception {
         mvc.perform(get("/")).andExpect(status().isOk());
     }
 }
